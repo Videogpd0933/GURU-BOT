17d1271dcb5eabdf8bd8069bbc3408d7d4a11c11
Videogpd0933 <149185771+Videogpd0933@users.noreply.github.com>
2023-11-22 19:54:29 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,9 +6,9 @@ import fetch from 'node-fetch'
 import axios from 'axios'
 
 
-global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+global.owner = [ MASUD
+  ['917679982058', 'MASUD', true],
+  ['917679982058', 'RANA', true], 
   [''] 
 ] //Number of owners
 
